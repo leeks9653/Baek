e9669c60cb8cf35d737681f611b99b4b46690fba
leeks9653 <leeks9653@naver.com>
2021-01-27 23:22:06 +0900

    2021-01-27

---
+++ Baek/src/main/Q_2750.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Q_2750 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		for (int i = arr.length; i >= 0; i--) {
+			for (int j = 0; j < i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		for (int t : arr) {
+			System.out.println(t);
+		}
+	}
+}
